d2548d2946cc33bce64df05df910e2896ec3ceaa
yuenshan <A00270323@mycambrian.ca>
2023-08-01 14:14:18 -0400

    init

---
+++ Recipe.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Recipe.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+

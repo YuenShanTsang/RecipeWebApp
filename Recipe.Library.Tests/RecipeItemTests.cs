618bb2ceae8cba8097bc18feed6818d76bce66a0
yuenshan <A00270323@mycambrian.ca>
2023-08-11 21:45:46 -0400

    Added 2 unit tests

---
+++ Recipe.Library.Tests/RecipeItemTests.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Recipe.Library.Models;
+
+[TestClass]
+public class RecipeItemTests
+{
+    [TestMethod]
+    public void RecipeItem_SerializationDeserialization_ShouldBeValid()
+    {
+        // Arrange
+        RecipeItem original = new RecipeItem
+        {
+            RecipeName = "Recipe 1",
+            RecipeCategory = "Beef"
+        };
+
+        // Act
+        string serialized = JsonConvert.SerializeObject(original);
+        RecipeItem deserialized = JsonConvert.DeserializeObject<RecipeItem>(serialized);
+
+        // Assert
+        Assert.AreEqual(original.RecipeName, deserialized.RecipeName);
+        Assert.AreEqual(original.RecipeCategory, deserialized.RecipeCategory);
+        // Verify other properties as needed
+    }
+}
--- Recipe.WebApp.Tests/HomeTests.cs
+++ Recipe.WebApp.Tests/HomeTests.cs
@@ -7,7 +7,7 @@ using WebDriverManager.DriverConfigs.Impl;
 namespace Recipe.WebApp.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class HomeTests
     {
         private IWebDriver? _webdriver;
         private const string BaseUrl = "https://localhost:5001/";
@@ -37,11 +37,40 @@ namespace Recipe.WebApp.Tests
             // Find the submit button
             var submitButton = _webdriver?.FindElement(By.CssSelector("form button[type='submit']"));
 
+            // Click using JavaScript to bypass any intercepting elements
+            ((IJavaScriptExecutor)_webdriver).ExecuteScript("arguments[0].click();", submitButton);
+
             // Navigate to Home page
             _webdriver?.Navigate().GoToUrl(BaseUrl);
 
             // Check if the created recipe name is displayed on the Home page
             Assert.IsTrue(_webdriver?.PageSource.Contains("Test Recipe"));
+        }
+
+
+        [TestMethod]
+        public void CheckDetailsPage()
+        {
+            // Navigate to Details page
+            _webdriver?.Navigate().GoToUrl(BaseUrl + "Home/Details/1");
+
+            // Use WebDriverWait to wait for the elements to become visible
+            var wait = new WebDriverWait(_webdriver, TimeSpan.FromSeconds(10));
+
+            try
+            {
+                // Wait for the recipe detail element to be visible
+                var recipeDetailElement = wait.Until(driver =>
+                    driver.FindElement(By.ClassName("recipe-detail")));
+
+                // Verify recipe name
+                var recipeNameElement = recipeDetailElement.FindElement(By.TagName("h1"));
+                Assert.IsTrue(recipeNameElement.Text.Contains("Test Recipe"));
+            }
+            catch (WebDriverTimeoutException)
+            {
+                Assert.Fail("The recipe detail element was not found on the details page.");
+            }
         }
 
         [TestCleanup]
